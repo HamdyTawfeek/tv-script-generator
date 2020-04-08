b740046d8ea680499a09ec8326dfb294b036cb38
Hamdy <hamdy.t@trufla.com>
2020-04-08 13:18:28 +0200

    helper functions in saving and loading the model

--- helper.py
+++ helper.py
@@ -1,4 +1,6 @@
 import os
+import pickle
+import torch
 
 
 SPECIAL_WORDS = {'PADDING': '<PAD>'}
@@ -41,3 +43,13 @@ def load_preprocess():
     Load the Preprocessed Training data and return them in batches of <batch_size> or less
     """
     return pickle.load(open('preprocess.p', mode='rb'))
+
+
+def save_model(filename, decoder):
+    save_filename = os.path.splitext(os.path.basename(filename))[0] + '.pt'
+    torch.save(decoder, save_filename)
+
+
+def load_model(filename):
+    save_filename = os.path.splitext(os.path.basename(filename))[0] + '.pt'
+    return torch.load(save_filename)
